de1a2ef32761c208137e5f18bf44da44bd936b79
MochTor <MochTor@users.noreply.github.com>
2016-03-19 14:57:18 +0100

    Applied main algorithm
    Written entire (draft) main function and init function

--- QR_serial.c
+++ QR_serial.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include <time.h>
 
 #include "QR_serial.h"
@@ -29,8 +30,29 @@ int int main(int argc, char const *argv[]) {
     start = clock();    //memorizing starting time
     A = (double*) malloc (m * n * sizeof(double));  //allocating memory for matrix A
     R = (double*) malloc (n * n * sizeof(double));  //allocating memory for matrix R
-    bzero(A, m*n);
-    bzero(R, n*n);
+    bzero(A, m*n);  //cleaning A's memory
+    bzero(R, n*n);  //cleaning R's memory
+    initMatrix(A, m, n);    //init A matrix with random values
+    gram(A, m, n, R);   //applying Gram-Schmidt algorithm
+    free(A);    //deallocating A's memory
+    free(R);    //deallocating R's memory
+    stop = clock(); //memorizing stop time
     //---------------------------------------------------
+
+    printf("Elapsed time %7.3f [s]\n", (stop-start)/(double)CLOCKS_PER_SEC);
+
     return 0;
+}
+
+void initMatrix(double *A, int m, int n) {
+    srand((unsigned int) 123);
+    for (int i = 0; i < m; i++) {
+        for (int ii = 0; i < n; i++) {
+            A[i][ii] = rand() % 100;
+        }
+    }
+}
+
+void gram(double* A, int M, int N, double *R) {
+    
 }
