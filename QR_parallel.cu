ba7574ddc3dc90020b4830a05863f84af0fabcf1
MochTor <MochTor@users.noreply.github.com>
2016-03-23 21:02:09 +0100

    Added time measurements for CUDA

--- QR_parallel.cu
+++ QR_parallel.cu
@@ -14,6 +14,8 @@
 
 #include "QR_parallel.h"
 
+#define THREADS_PER_BLOCK 512   //I'll use 512 threads for each block (as required in the assignment)
+
 int main(int argc, char const *argv[]) {
     //--------------- Defining variables ---------------
     int m = 400;    //matrix rows number (first set)
@@ -21,13 +23,16 @@ int main(int argc, char const *argv[]) {
     // int m = 1000;    //matrix rows number (second set)
     // int n = 800;     //matrix columns number (second set)
 
-    double *A, *R;  //A is the initial matrix, R the upper triangular matrix
+    double *A_h, *R_h;  //A is the initial matrix, R the upper triangular matrix. Copy on host (_h)
 
-    
+    cudaEvent_t start, stop;    //CUDA events to record start and stop time
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    float elapsedTime;  //it memorizes the overall elapsed time since the start of CUDA code
     //---------------------------------------------------
 
     //--------------- Starting algoritm -----------------
-    start = clock();    //memorizing starting time
+    cudaEventRecord(start, 0);    //memorizing starting time
     A = (double*) malloc (m * n * sizeof(double));  //allocating memory for matrix A
     R = (double*) malloc (n * n * sizeof(double));  //allocating memory for matrix R
     bzero(A, m*n);  //cleaning A's memory
@@ -38,10 +43,12 @@ int main(int argc, char const *argv[]) {
     gram(A, m, n, R);   //applying Gram-Schmidt algorithm
     free(A);    //deallocating A's memory
     free(R);    //deallocating R's memory
-    stop = clock(); //memorizing stop time
+    cudaEventRecord(stop, 0); //memorizing stop time
+    cudaEventSynchronize(stop);
     //---------------------------------------------------
 
-    printf("Elapsed time %7.5f [s]\n", (stop-start)/(double)CLOCKS_PER_SEC);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Elapsed time %7.5f [s]\n", elapsedTime/1000);
 
     return 0;
 }
@@ -60,38 +67,38 @@ void initMatrixZero(double *A, int m, int n) {
     }
 }
 
-void gram(double* A, int m, int n, double *R) {
-    double sf;  //Scale factor
-
-    for (int ii = 0; ii < n-1; ii++) {
-        xTA(&R[ii*n + ii], n-ii, &A[ii], m, n, &A[ii], n);  //1
-        sf = sqrt(R[ii*n + ii]);                            //2
-        scale(&A[ii], m, n, sf);                            //3
-        scale(&R[ii*n + ii], n, 1, sf);                     //4
-        r1_update(&A[ii+1], m, n-ii-2, n, &A[ii], n, &R[ii]);//5
-    }
-}
-
-void xTA (double *y, int k, double*A, int m, int lda, double *x, int ldx) {
-    double s;   //It memorizes the sum
-
-    for (int jj = 0; jj < k; jj++) {    //Moving through columns
-        s = 0;
-        for (int ii = 0; ii < m; ii++) {    //Moving through rows
-            s += x[ii * ldx] * A[ii*lda + jj];
-        }
-        y[jj] = s;  //Adding the sum to result vector
-    }
-}
-
-void scale(double *d, int m, int ld, double s) {
-    for (int ii = 0; ii < m; ii++)    //Moving through rows
-        d[ii*ld] = d[ii*ld] / s;    //Applying scale
-}
-
-void r1_update(double *A, int m, int n, int lda, double *col, int ldc, double *row) {
-    //Does it work? Recheck!
-    for (int jj = 0; jj < n-1; jj++)  //Moving through columns
-        for (int ii = 0; ii < m; ii++)   //Moving through rows
-            A[lda*ii + jj+1] -= row[jj] * col[ldc*ii];
-}
+// void gram(double* A, int m, int n, double *R) {
+//     double sf;  //Scale factor
+//
+//     for (int ii = 0; ii < n-1; ii++) {
+//         xTA(&R[ii*n + ii], n-ii, &A[ii], m, n, &A[ii], n);  //1
+//         sf = sqrt(R[ii*n + ii]);                            //2
+//         scale(&A[ii], m, n, sf);                            //3
+//         scale(&R[ii*n + ii], n, 1, sf);                     //4
+//         r1_update(&A[ii+1], m, n-ii-2, n, &A[ii], n, &R[ii]);//5
+//     }
+// }
+//
+// void xTA (double *y, int k, double*A, int m, int lda, double *x, int ldx) {
+//     double s;   //It memorizes the sum
+//
+//     for (int jj = 0; jj < k; jj++) {    //Moving through columns
+//         s = 0;
+//         for (int ii = 0; ii < m; ii++) {    //Moving through rows
+//             s += x[ii * ldx] * A[ii*lda + jj];
+//         }
+//         y[jj] = s;  //Adding the sum to result vector
+//     }
+// }
+//
+// void scale(double *d, int m, int ld, double s) {
+//     for (int ii = 0; ii < m; ii++)    //Moving through rows
+//         d[ii*ld] = d[ii*ld] / s;    //Applying scale
+// }
+//
+// void r1_update(double *A, int m, int n, int lda, double *col, int ldc, double *row) {
+//     //Does it work? Recheck!
+//     for (int jj = 0; jj < n-1; jj++)  //Moving through columns
+//         for (int ii = 0; ii < m; ii++)   //Moving through rows
+//             A[lda*ii + jj+1] -= row[jj] * col[ldc*ii];
+// }
--- QR_parallel.h
+++ QR_parallel.h
@@ -1,12 +1,12 @@
 /**
- * Header file for serial QR factorization program used for Parallel Computing class
+ * Header file for parallel QR factorization program used for Parallel Computing class
  *
  * 2016 Marco Tieghi - marco01.tieghi@student.unife.it
  *
  */
 
-#ifndef QRSERIAL_H
-#define QRSERIAL_H
+#ifndef QRPARALLEL_H
+#define QRPARALLEL_H
 
 /**
  * Initialize a matrix, according to text
@@ -30,7 +30,7 @@ void initMatrixZero(double *A, int m, int n);
   * col   m x 1 ldc
   * coeff 1 x n
   */
-void r1_update(double *A, int m, int n, int lda, double *col, int ldc, double *row);
+// void r1_update(double *A, int m, int n, int lda, double *col, int ldc, double *row);
 
 
 /**
@@ -40,7 +40,7 @@ void r1_update(double *A, int m, int n, int lda, double *col, int ldc, double *r
   * x : m, leading dim. ldx
   * y : 1 x k
   */
-void xTA (double *y, int k, double*A, int m, int lda, double *x, int ldx);
+// void xTA (double *y, int k, double*A, int m, int lda, double *x, int ldx);
 
 /**
   * Mult. for constant s
@@ -49,7 +49,7 @@ void xTA (double *y, int k, double*A, int m, int lda, double *x, int ldx);
   * ld leading dimension (distance from elements)
   *
   */
-void scale(double *d, int m, int ld, double s);
+// void scale(double *d, int m, int ld, double s);
 
 /**
   * Performs Modified Gram Schmidt
@@ -59,6 +59,6 @@ void scale(double *d, int m, int ld, double s);
   * R n x n
   * m >= n
   */
-void gram(double* A, int m, int n, double *R);
+// void gram(double* A, int m, int n, double *R);
 
 #endif
